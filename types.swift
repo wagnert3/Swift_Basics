6d989623034bdf6826ec3cc2a5caa03872c5539c
Wagner Santiago <66319302+wagnert3@users.noreply.github.com>
2024-01-14 20:53:28 -0400

    Types Class

---
+++ types.swift
@@ -0,0 +1,23 @@
+import Foundation // Foundation is a framework that contains all the basic data types such as String, Int, Double, Array, etc.
+import UIKit // UI Kit is a framework that contains all the UI elements such as buttons, labels, etc.
+
+// Types
+
+var x = 3 // Int
+var name = "Joe" // String
+
+var lastName: String = "123"
+let age: Int = 22
+let price: Double = 1.99
+
+var height: Float = 200.1232131123123
+
+class Person {
+
+}
+
+var jong: Person = Person()
+
+var button1: UIButton = UIButton()
+
+var isComplete: Bool = false
